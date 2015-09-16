31110e7c3028957283d6b94c528d4f51f252c71d
aardappel <aardappel@gmail.com>
2015-09-15 18:11:06 -0700

    building freetype from source on Android

--- dev/android/jni/src/Android.mk
+++ dev/android/jni/src/Android.mk
@@ -4,19 +4,25 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := main
 
-SDL_PATH := ../../../../external/sdl
 LOBSTER_PATH := ../../..
 
-LOCAL_C_INCLUDES := $(SDL_PATH)/include $(LOBSTER_PATH)/src $(LOCAL_PATH)/$(LOBSTER_PATH)/include
+SDL_PATH := ../../../../external/sdl
+FREETYPE_PATH := $(LOBSTER_PATH)/external/freetype
 
-LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.c \
+LOCAL_C_INCLUDES := \
+    $(SDL_PATH)/include \
+    $(LOCAL_PATH)/$(FREETYPE_PATH)/include \
+    $(LOBSTER_PATH)/src \
+    $(LOCAL_PATH)/$(LOBSTER_PATH)/include
+
+LOCAL_SRC_FILES := \
 	$(LOBSTER_PATH)/src/main.cpp \
     $(LOBSTER_PATH)/src/compiler.cpp \
 	$(LOBSTER_PATH)/src/audio.cpp \
 	$(LOBSTER_PATH)/src/builtins.cpp \
 	$(LOBSTER_PATH)/src/file.cpp \
 	$(LOBSTER_PATH)/src/font.cpp \
-	$(LOBSTER_PATH)/src/ftsystem.cpp \
+	$(LOBSTER_PATH)/src/fontrenderer.cpp \
 	$(LOBSTER_PATH)/src/glgeom.cpp \
 	$(LOBSTER_PATH)/src/glloadiqm.cpp \
 	$(LOBSTER_PATH)/src/glshader.cpp \
@@ -30,12 +36,35 @@ LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.c \
 	$(LOBSTER_PATH)/src/sdlsystem.cpp \
 	$(LOBSTER_PATH)/src/simplex.cpp \
 	$(LOBSTER_PATH)/src/stdafx.cpp \
-	$(LOBSTER_PATH)/src/vmdata.cpp
+	$(LOBSTER_PATH)/src/vmdata.cpp \
+    $(FREETYPE_PATH)/src/autofit/autofit.c \
+	$(FREETYPE_PATH)/src/base/basepic.c \
+	$(FREETYPE_PATH)/src/base/ftapi.c \
+	$(FREETYPE_PATH)/src/base/ftbase.c \
+	$(FREETYPE_PATH)/src/base/ftbbox.c \
+	$(FREETYPE_PATH)/src/base/ftbitmap.c \
+	$(FREETYPE_PATH)/src/base/ftdbgmem.c \
+	$(FREETYPE_PATH)/src/base/ftdebug.c \
+	$(FREETYPE_PATH)/src/base/ftglyph.c \
+	$(FREETYPE_PATH)/src/base/ftinit.c \
+	$(FREETYPE_PATH)/src/base/ftpic.c \
+	$(FREETYPE_PATH)/src/base/ftstroke.c \
+	$(FREETYPE_PATH)/src/base/ftsynth.c \
+	$(FREETYPE_PATH)/src/base/ftsystem.c \
+	$(FREETYPE_PATH)/src/cff/cff.c \
+	$(FREETYPE_PATH)/src/pshinter/pshinter.c \
+	$(FREETYPE_PATH)/src/psnames/psnames.c \
+	$(FREETYPE_PATH)/src/raster/raster.c \
+	$(FREETYPE_PATH)/src/sfnt/sfnt.c \
+	$(FREETYPE_PATH)/src/smooth/smooth.c \
+	$(FREETYPE_PATH)/src/truetype/truetype.c \
+    $(SDL_PATH)/src/main/android/SDL_android_main.c
 
 LOCAL_SHARED_LIBRARIES := SDL2
-LOCAL_STATIC_LIBRARIES := freetype2-static
+LOCAL_STATIC_LIBRARIES :=
 
-LOCAL_CFLAGS := -std=c++11
+LOCAL_CPPFLAGS := -std=c++11
+LOCAL_CFLAGS := -DFT2_BUILD_LIBRARY=1
 
 LOCAL_LDLIBS := -lGLESv1_CM -lGLESv2 -llog
 
