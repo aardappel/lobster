0149b4991911b6079242ece1fdb97c1d77e7eea1
Bart van der Werf <bluelive@gmail.com>
2022-06-06 20:12:39 +0200

    When the warning hasn't been seen before, write it out and add it to the list of seen warnings. (#180)

--- dev/src/lobster/typecheck.h
+++ dev/src/lobster/typecheck.h
@@ -387,12 +387,9 @@ struct TypeChecker {
                 if (a->exptype->t == V_INT) {
                     if (auto ic = Is<IntConstant>(a)) {
                         auto this_warn = make_pair(ic->line, ic->integer);
-                        if (integer_literal_warnings.find(this_warn) ==
-                            integer_literal_warnings.end()) {
+                        if (integer_literal_warnings.insert(this_warn).second) {
                             parser.WarnAt(a, "integer literal (", ic->integer,
                                           ") where float expected");
-                        } else {
-                            integer_literal_warnings.insert(this_warn);
                         }
                     }
                     MakeFloat(a);
