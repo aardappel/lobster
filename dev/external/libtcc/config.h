6684aa95dcf78fd6c2839c7eb4f8c9a880f35ccb
Wouter van Oortmerssen <aardappel@gmail.com>
2022-06-28 20:18:16 -0700

    New version of libtcc 2022-05-26

--- dev/external/libtcc/config.h
+++ dev/external/libtcc/config.h
@@ -1,1 +1,16 @@
 #define TCC_VERSION "0.9.27"
+
+#if defined(__x86_64__)
+    #define TCC_TARGET_X86_64 1
+#elif defined(__aarch64__)
+    #define TCC_TARGET_ARM64 1
+#elif defined(__arm__)
+    #define TCC_TARGET_ARM 1
+#elif defined(__i386__)
+    #define TCC_TARGET_I386 1
+#else
+    #define TCC_TARGET_X86_64 1
+#endif
+
+#define TCC_IS_NATIVE 1
+#define CONFIG_TCC_PREDEFS 1
