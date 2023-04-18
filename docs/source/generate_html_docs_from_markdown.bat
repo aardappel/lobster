40d6f1aebc52f84cdc53543a73a1ad2443c6793d
Ben Smith <binjimin@gmail.com>
2023-04-18 14:18:51 -0700

    Add favicon to docs (#240)

--- docs/source/generate_html_docs_from_markdown.bat
+++ docs/source/generate_html_docs_from_markdown.bat
@@ -1,3 +1,3 @@
-for %%F in (*.md) do "C:\Program Files\Pandoc\pandoc.exe" -s -f markdown -t html5 -c github.css -o ..\%%~nF.html %%F
+for %%F in (*.md) do "C:\Program Files\Pandoc\pandoc.exe" -s -f markdown -t html5 -c github.css -H head.html -o ..\%%~nF.html %%F
 rem for %%F in (*.md) do multimarkdown.exe -o ..\%%~nF.html mmd_header.txt %%F
 pause
--- docs/source/generate_html_docs_from_markdown.sh
+++ docs/source/generate_html_docs_from_markdown.sh
@@ -1,5 +1,5 @@
 for file in *.md
 do
     base=${file%.*}
-    pandoc "$file" -o "../$base".html -s -f markdown -t html5 -c github.css
+    pandoc "$file" -o "../$base".html -s -f markdown -t html5 -c github.css -H head.html
 done
