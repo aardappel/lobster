e7647f034279e366122cc430050393acdad8607b
WraithGlade <8360085+WraithGlade@users.noreply.github.com>
2024-03-09 19:54:26 -0500

    Added the `-smart` command option to the `.sh` variant of the Markdown to HTML doc converter script, to match the behavior the Windows batch script.

--- docs/source/generate_html_docs_from_markdown.sh
+++ docs/source/generate_html_docs_from_markdown.sh
@@ -1,5 +1,5 @@
 for file in *.md
 do
     base=${file%.*}
-    pandoc "$file" -o "../$base".html -s -f markdown -t html5 -c github.css -H head.html
+    pandoc "$file" -o "../$base".html -s -f markdown-smart -t html5 -c github.css -H head.html
 done
