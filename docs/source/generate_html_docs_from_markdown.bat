58c9345c8ce97072b0b7c886781da38adcf34d0e
WraithGlade <8360085+WraithGlade@users.noreply.github.com>
2024-03-09 19:30:15 -0500

    Changed the Pandoc batch file command to disable all automatic conversion of characters into special context-based variants.
    Now (with the setting *off*) Pandoc instead leaves all characters exactly as they were (i.e. this change makes Pandoc's output more ASCII-like).
    
    Consequentially, this fixes the substitution of "..." to be one unicode character, so that it is now left alone and remains 3 characters.
    
    This also changes many other special characters throughout the generated documentation, but perhaps this is desirable for easier copy pasting by programmers since it will less often create sometimes unwanted unicode characters.

--- docs/source/generate_html_docs_from_markdown.bat
+++ docs/source/generate_html_docs_from_markdown.bat
@@ -1,2 +1,2 @@
-for %%F in (*.md) do pandoc -s -f markdown -t html5 -c github.css -H head.html -o ..\%%~nF.html %%F
+for %%F in (*.md) do pandoc -s -f markdown-smart -t html5 -c github.css -H head.html -o ..\%%~nF.html %%F
 pause
