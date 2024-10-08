60eb23716a30caa5263afa42aaa59919c9a2ac1a
Wouter van Oortmerssen <aardappel@gmail.com>
2024-10-07 20:19:12 -0700

    Fixed test scripts running unittest multiple times

--- dev/lobster/test_all_compile_only_verbose_no_projects.bat
+++ dev/lobster/test_all_compile_only_verbose_no_projects.bat
@@ -1,6 +1,6 @@
 @rem @echo off
 @cls
-@for /R ..\.. %%F in (modules\*.lobster) do @(@start /B ..\..\bin\lobster.exe --compile-only %%F)
-@for /R ..\.. %%F in (tests\*.lobster) do @(@start /B ..\..\bin\lobster.exe --compile-only %%F)
-@for /R ..\.. %%F in (samples\*.lobster) do @(@start /B ..\..\bin\lobster.exe --compile-only %%F)
+@start /B ..\..\bin\lobster.exe --compile-only ..\..\tests\unittest.lobster
+@for /R ..\..\modules\ %%F in (*.lobster) do @(@start /B ..\..\bin\lobster.exe --compile-only %%F)
+@for /R ..\..\samples\ %%F in (*.lobster) do @(@start /B ..\..\bin\lobster.exe --compile-only %%F)
 @pause
--- dev/lobster/test_all_no_projects.bat
+++ dev/lobster/test_all_no_projects.bat
@@ -1,5 +1,6 @@
 @rem @echo off
 @cls
+@start /B ..\..\bin\lobster.exe --non-interactive-test --silent ..\..\tests\unittest.lobster
 @for /R ..\.. %%F in (modules\*.lobster) do @(@start /B ..\..\bin\lobster.exe --non-interactive-test --silent %%F)
 @for /R ..\.. %%F in (tests\*.lobster) do @(@start /B ..\..\bin\lobster.exe --non-interactive-test --silent %%F)
 @for /R ..\.. %%F in (samples\*.lobster) do @(@start /B ..\..\bin\lobster.exe --non-interactive-test --silent %%F)
