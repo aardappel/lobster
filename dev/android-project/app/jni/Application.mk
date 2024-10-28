bd65a7bee960047c56c7b74f39d37500f8ed1bfc
Wouter van Oortmerssen <aardappel@gmail.com>
2024-10-27 18:53:17 -0700

    Attempted fix for Android

--- dev/android-project/app/jni/Application.mk
+++ dev/android-project/app/jni/Application.mk
@@ -8,4 +8,5 @@ APP_ABI := arm64-v8a # armeabi-v7a x86 x86_64
 # Min runtime API level (ES3.0 == 18)
 APP_PLATFORM=android-18
 
-APP_CPPFLAGS += -frtti -std=c++17 -Wno-c++17-extensions -fexceptions -Wno-switch
+APP_CPPFLAGS += -fsigned-char -frtti -std=c++17 -Wno-c++17-extensions -fexceptions -Wno-switch
+APP_CFLAGS += -fsigned-char
