db15fc8850376efdc3feb5f9b984959bbbde683c
Wouter van Oortmerssen <aardappel@gmail.com>
2025-05-23 09:45:26 -0700

    Attempt to build for multiple architectures on OSX CI

--- dev/build_osx.sh
+++ dev/build_osx.sh
@@ -1,5 +1,6 @@
 mkdir -p xcode-cmake
 cd xcode-cmake
 # FIXME make joystick stuff compile?
-cmake .. -G Xcode -DSDL_HIDAPI_JOYSTICK=OFF -DSDL_JOYSTICK=OFF -DSDL_HAPTIC=OFF
+usex86="${1:-OFF}"
+cmake .. -DLOBSTER_OSX_X86=$usex86 -G Xcode -DSDL_HIDAPI_JOYSTICK=OFF -DSDL_JOYSTICK=OFF -DSDL_HAPTIC=OFF
 cd ..
