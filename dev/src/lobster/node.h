56bc8035d1664a02959f88bdb167836d4de86123
Wouter van Oortmerssen <aardappel@gmail.com>
2026-08-28 21:57:06 -0700

    Temp fix for pakfile string handling and "expression statement has no effect" warning

--- dev/src/lobster/node.h
+++ dev/src/lobster/node.h
@@ -390,6 +390,7 @@ struct StringConstant : Node {
         return str == ((StringConstant *)o)->str;
     }
     SHARED_SIGNATURE(StringConstant, TName(T_STR), false)
+    STATEMENTMETHOD  // FIXME: this is hear because of how T_PAKFILE is implemented, ideally should be removed.
 };
 
 struct EnumRef : Node {
